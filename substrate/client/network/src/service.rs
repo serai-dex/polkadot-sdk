41e5af15e73dd22ca91c9fd3c12bfbe82bb8ed4f
Aaro Altonen <aaro.altonen@parity.io>
2023-10-30 12:42:36 +0200

    wip

--- substrate/client/network/src/service.rs
+++ substrate/client/network/src/service.rs
@@ -73,6 +73,7 @@ use libp2p::{
 use log::{debug, error, info, trace, warn};
 use metrics::{Histogram, MetricSources, Metrics};
 use parking_lot::Mutex;
+use schnellru::{ByLength, LruMap};
 
 use sc_network_common::{
 	role::{ObservedRole, Roles},
@@ -526,7 +527,7 @@ where
 			boot_node_ids,
 			reported_invalid_boot_nodes: Default::default(),
 			peer_store_handle: params.peer_store,
-			notif_protocol_handles,
+			address_scores: LruMap::new(ByLength::new(128)),
 			_marker: Default::default(),
 			_block: Default::default(),
 		})
@@ -1196,8 +1197,8 @@ where
 	reported_invalid_boot_nodes: HashSet<PeerId>,
 	/// Peer reputation store handle.
 	peer_store_handle: PeerStoreHandle,
-	/// Notification protocol handles.
-	notif_protocol_handles: Vec<protocol::ProtocolHandle>,
+	/// Address scores for tracking external addresses.
+	address_scores: LruMap<Multiaddr, usize>,
 	/// Marker to pin the `H` generic. Serves no purpose except to not break backwards
 	/// compatibility.
 	_marker: PhantomData<H>,
@@ -1416,10 +1417,20 @@ where
 					);
 				}
 				self.peer_store_handle.add_known_peer(peer_id);
-				// Confirm the observed address manually since they are no longer trusted by
-				// default (libp2p >= 0.52)
+
 				// TODO: remove this when/if AutoNAT is implemented.
-				self.network_service.add_external_address(observed_addr);
+				match self.address_scores.get(&observed_addr) {
+					Some(value) => {
+						*value = value.saturating_add(1);
+
+						if *value >= 8 {
+							self.network_service.add_external_address(observed_addr);
+						}
+					},
+					None => {
+						self.address_scores.insert(observed_addr.clone(), 1usize);
+					},
+				}
 			},
 			SwarmEvent::Behaviour(BehaviourOut::Discovered(peer_id)) => {
 				self.peer_store_handle.add_known_peer(peer_id);
